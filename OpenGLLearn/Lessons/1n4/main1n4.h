118572051c438a144057f7e9e38f6603d6161396
codemanfix <codemanfix@mail.ru>
2020-05-27 15:50:49 +0300

    + hello rectangle!

--- OpenGLLearn/Lessons/1n4/main1n4.h
+++ OpenGLLearn/Lessons/1n4/main1n4.h
@@ -90,12 +90,20 @@ namespace lesson_1n4
 		glDeleteShader(vertexShader);
 		glDeleteShader(fragmentShader);
 
-		//вершины треугольника
 		GLfloat vertices[] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+			 0.5f,  0.5f, 0.0f,  // Верхний правый угол
+			 0.5f, -0.5f, 0.0f,  // Нижний правый угол
+			-0.5f, -0.5f, 0.0f,  // Нижний левый угол
+			-0.5f,  0.5f, 0.0f   // Верхний левый угол
 		};
+
+		GLuint indices[] = {  // Помните, что мы начинаем с 0!
+			0, 1, 3,   // Первый треугольник
+			1, 2, 3    // Второй треугольник
+		};
+
+		GLuint EBO;
+		glGenBuffers(1, &EBO);
 
 		GLuint VBO;
 		glGenBuffers(1, &VBO);
@@ -104,12 +112,18 @@ namespace lesson_1n4
 		glGenVertexArrays(1, &VAO);
 		glBindVertexArray(VAO);
 
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 		glEnableVertexAttribArray(0);
 
 		glBindVertexArray(0);
+
+		//wireframe
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 		//игровой цикл
 		while (!glfwWindowShouldClose(window))
@@ -120,7 +134,7 @@ namespace lesson_1n4
 			// Команды отрисовки здесь
 			glUseProgram(shaderProgram);
 			glBindVertexArray(VAO);
-			glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 			glBindVertexArray(0);
 
 			// Меняем буферы местами
