5b824a1b0478a08f49bb01ba3d1eca945b722461
codemanfix <codemanfix@mail.ru>
2020-05-31 16:39:49 +0300

    + lesson 4.1 show linear representation of depth buffer

--- OpenGLLearn/Lessons/4n1_depth_testing/shaders/depth_testing.fs
+++ OpenGLLearn/Lessons/4n1_depth_testing/shaders/depth_testing.fs
@@ -1,11 +1,19 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoords;
-
-uniform sampler2D texture1;
+float zNear = 0.1; 
+float zFar  = 100.0; 
+  
+float LinearizeDepth(float depth) 
+{
+    // преобразуем обратно в NDC
+    float z = depth * 2.0 - 1.0; 
+    return (2.0 * zNear * zFar) / (zFar + zNear - z * (zFar - zNear));	
+}
 
 void main()
-{    
-    FragColor = texture(texture1, TexCoords);
+{             
+    // деление на zFar для лучшей наглядности
+    float depth = LinearizeDepth(gl_FragCoord.z) / zFar;
+    FragColor = vec4(vec3(depth), 1.0);
 }
