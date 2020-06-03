2f98d91c3195875fdbe45df9a52275b0f9a661d4
codemanfix <codemanfix@mail.ru>
2020-06-03 14:47:52 +0300

    + lesson 4.10 using intancing array

--- OpenGLLearn/Lessons/4n10_instansing/main4n10.h
+++ OpenGLLearn/Lessons/4n10_instansing/main4n10.h
@@ -45,6 +45,28 @@ namespace lesson_4n10
 		lesson_1n5::CShader sceneShader;
 		if (!sceneShader.Init("Lessons/4n10_instansing/shaders/scene.vs", "Lessons/4n10_instansing/shaders/scene.fs")) return -1;
 
+		glm::vec2 translations[100];
+		int index = 0;
+		float offset = 0.1f;
+		for (int y = -10; y < 10; y += 2)
+		{
+			for (int x = -10; x < 10; x += 2)
+			{
+				glm::vec2 translation;
+				translation.x = (float)x / 10.0f + offset;
+				translation.y = (float)y / 10.0f + offset;
+				translations[index++] = translation;
+			}
+		}
+
+		// store instance data in an array buffer
+		// --------------------------------------
+		unsigned int instanceVBO;
+		glGenBuffers(1, &instanceVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 		float quadVertices[] = {
 			// positions     // colors
 			-0.05f,  0.05f,  1.0f, 0.0f, 0.0f,
@@ -62,32 +84,17 @@ namespace lesson_4n10
 		glGenBuffers(1, &quadVBO);
 		glBindVertexArray(quadVAO);
 		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(1);
 		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
-		glBindVertexArray(0);
-
-		glm::vec2 translations[100];
-		int index = 0;
-		float offset = 0.1f;
-		for (int y = -10; y < 10; y += 2)
-		{
-			for (int x = -10; x < 10; x += 2)
-			{
-				glm::vec2 translation;
-				translation.x = (float)x / 10.0f + offset;
-				translation.y = (float)y / 10.0f + offset;
-				translations[index++] = translation;
-			}
-		}
-
-		sceneShader.Use();
-		for (unsigned int i = 0; i < 100; i++)
-		{
-			sceneShader.setVec2f(("offsets[" + std::to_string(i) + "]").c_str(), translations[i]);
-		}
+		// also set instance data
+		glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ARRAY_BUFFER, instanceVBO); // this attribute comes from a different vertex buffer
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glVertexAttribDivisor(2, 1); // tell OpenGL this is an instanced vertex attribute.
 
 		float deltaTime;
 
@@ -100,8 +107,9 @@ namespace lesson_4n10
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+			sceneShader.Use();
 			glBindVertexArray(quadVAO);
-			glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 100);
+			glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 100); // 100 triangles of 6 vertices each
 			glBindVertexArray(0);
 
 			glfwSwapBuffers(window);
@@ -110,7 +118,7 @@ namespace lesson_4n10
 
 		glDeleteVertexArrays(1, &quadVAO);
 		glDeleteBuffers(1, &quadVBO);
-
+		glDeleteBuffers(1, &instanceVBO);
 		glfwTerminate();
 		return 0;		
 	}
