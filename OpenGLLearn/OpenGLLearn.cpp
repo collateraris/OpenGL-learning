92630e85e97cfca9c13e4519a81ddbf425ac6ac0
codemanfix <codemanfix@mail.ru>
2020-06-05 16:14:58 +0300

    + lesson 5.1 blinn_fong lighting

--- OpenGLLearn/OpenGLLearn.cpp
+++ OpenGLLearn/OpenGLLearn.cpp
@@ -1,11 +1,9 @@
 ﻿
-//#include "Lessons/4n11_anti_aliasing/main4n11.h"
-#include "OpenCL/1n1_init/opencl_main1n1.h"
+#include "Lessons/5n1_advanced_lighting/main5n1.h"
 
 int main()
 {
-	return opencl_1n1::lesson_main();
-	//return lesson_4n11::lesson_main();
+	return lesson_5n1::lesson_main();
 }
 
 
