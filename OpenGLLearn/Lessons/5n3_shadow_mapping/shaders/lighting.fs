e6e1cf76f6437eb9cd2fc6d43955a446195f074b
codemanfix <codemanfix@mail.ru>
2020-06-07 20:10:02 +0300

    + lesson 5.3 soft shadow with PCF (Percentage-closer filtering)

--- OpenGLLearn/Lessons/5n3_shadow_mapping/shaders/lighting.fs
+++ OpenGLLearn/Lessons/5n3_shadow_mapping/shaders/lighting.fs
@@ -43,7 +43,19 @@ float ShadowCalculation()
 	
 	float bias = max(0.05 * (1.0 - dot(lightDir, normal)), 0.05);
 	
-	float shadow =  currentDepth - bias > closestDepth ? 1.0 : 0.0;
+	float shadow = 0.0;
+	vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+	
+	for(int x = -1; x <= 1; ++x)
+	{
+		for(int y = -1; y <= 1; ++y)
+		{
+			float pcfDepth = texture(shadowMap, proj.xy + vec2(x, y) * texelSize).r;
+			shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;
+		}
+	}
+	
+	shadow /= 9.0;
 	
 	return shadow;
 }
